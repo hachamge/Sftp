49b7c075448cdfc579842f4c9b228f2e0a5e6383
Seymour Roberts <seymour2@pdx.edu>
2024-08-12 11:42:09 -0700

    highlight edited files, and readjust the assets/table size

--- static/script/assets.js
+++ static/script/assets.js
@@ -7,16 +7,18 @@ document.querySelectorAll(".editable").forEach((element) => {
 
   element.addEventListener("blur", (event) => {
     const newpath = event.target.innerText;
+    event.target.innerHTML = `<b style="border: 2px solid #ddd; padding: 5px;">${newpath}</b>`;
+    event.target.style.color = "purple";
     if (oldpath !== newpath) {
       console.log(`Filename changed from "${oldpath}" to "${newpath}"`);
 
-			fetch("/edit", {
-				method: 'POST',
-				body: JSON.stringify({oldpath: oldpath, newpath: newpath}),
-				headers: {
-					"Content-Type": "application/json",
-				}
-			});
+      fetch("/edit", {
+        method: "POST",
+        body: JSON.stringify({ oldpath: oldpath, newpath: newpath }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
     }
   });
 });
