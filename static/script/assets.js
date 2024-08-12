9b6477043976b37647d1022ceeebdde80d605b85
Seymour Roberts <seymour2@pdx.edu>
2024-08-12 14:00:58 -0700

    enable edit highlight stricky on change, disable spell check

--- static/script/assets.js
+++ static/script/assets.js
@@ -7,9 +7,9 @@ document.querySelectorAll(".editable").forEach((element) => {
 
   element.addEventListener("blur", (event) => {
     const newpath = event.target.innerText;
-    event.target.innerHTML = `<b style="border: 2px solid #ddd; padding: 5px;">${newpath}</b>`;
-    event.target.style.color = "purple";
     if (oldpath !== newpath) {
+    event.target.style.color = "purple";
+    event.target.innerHTML = `<b style="border: 2px solid #ddd; padding: 5px;">${newpath}</b>`;
       console.log(`Filename changed from "${oldpath}" to "${newpath}"`);
 
       fetch("/edit", {
